1a96f8c7f59b6aa9e68274a3f0a0b78eb4cc6b2f
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-04-20 19:19:06 +0100

    adding middelware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from db.database import Base, engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.router import router
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 Base.metadata.create_all(bind=engine)
 
